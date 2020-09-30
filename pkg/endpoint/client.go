fff2111ac124a1b6da3229a16b870d7de503403e
zizon <zzdtsv@gmail.com>
2020-10-01 00:48:34 +0800

    revise client,use plain rest api

--- pkg/endpoint/client.go
+++ pkg/endpoint/client.go
@@ -1,78 +1,48 @@
 package endpoint
 
 import (
+	"fmt"
 	"net/http"
-	"reflect"
+	"net/url"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	runtime "k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
 )
 
 type Client interface {
-	NewRequest(apiPath string, gv schema.GroupVersion) *rest.Request
+	Get(apiPath string) (*http.Response, error)
 }
 
 type client struct {
-	config rest.Config
-	http   *http.Client
-	scheme *runtime.Scheme
+	http.RoundTripper
+	host   string
+	header http.Header
 }
 
 func NewAPIClient(config Config) (Client, error) {
-	scheme := runtime.NewScheme()
-
-	config.Rest.APIPath = "/apis"
-	config.Rest.GroupVersion = &schema.GroupVersion{}
-	config.Rest.NegotiatedSerializer = serializer.NewCodecFactory(scheme)
-
 	rt, err := rest.TransportFor(&config.Rest)
-
 	if err != nil {
 		klog.Errorf("fail create client with config: %v reason: %v", config, err)
-		return client{}, err
+		return &client{}, err
 	}
 	client := client{
-		config: config.Rest,
-		http:   &http.Client{Transport: rt},
-		scheme: scheme,
+		rt,
+		config.Rest.Host,
+		http.Header{
+			"Authorization": []string{fmt.Sprintf("Bearer %s", (&config.Rest).BearerToken)},
+		},
 	}
-
-	//config.Rest.NegotiatedSerializer = serializer.NewCodecFactory(client.scheme)
 	return client, nil
 }
 
-func (client client) Register(objType reflect.Type, gv schema.GroupVersion) Client {
-	gvk := gv.WithKind(objType.Name())
-
-	metav1.AddToGroupVersion(client.scheme, gv)
-	client.scheme.AddKnownTypeWithName(gvk, &unstructured.Unstructured{})
-	return client
-}
-
-func (client client) NewRequest(apiPath string, gv schema.GroupVersion) *rest.Request {
-	base, versionurl, err := rest.DefaultServerURL(
-		client.config.Host,
-		apiPath,
-		gv,
-		client.config.Insecure,
-	)
-	if err != nil {
-		klog.Errorf("malform request:%v", err)
-		return &rest.Request{}
-	}
-
-	return rest.NewRequestWithClient(
-		base, versionurl, rest.ClientContentConfig{
-			AcceptContentTypes: client.config.AcceptContentTypes,
-			ContentType:        client.config.ContentType,
-			GroupVersion:       gv,
-			Negotiator:         runtime.NewClientNegotiator(client.config.NegotiatedSerializer, gv),
-		}, client.http,
-	)
-	//return rest.NewRequest(client.delegate)
+func (client client) Get(apiPath string) (*http.Response, error) {
+	return client.RoundTrip(&http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   client.host,
+			Path:   apiPath,
+		},
+		Header: client.header,
+	})
 }
--- pkg/endpoint/client_test.go
+++ pkg/endpoint/client_test.go
@@ -1,16 +1,10 @@
 package endpoint
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"io/ioutil"
-	"net/http"
-	"net/url"
 	"testing"
 
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/rest"
 	"k8s.io/klog"
 )
 
@@ -32,27 +26,7 @@ func TestClient(t *testing.T) {
 		return
 	}
 
-	response, err := client.NewRequest("/api", schema.GroupVersion{Version: "v1"}).Verb("GET").Resource("namespaces").
-		DoRaw(context.TODO())
-	if err != nil {
-		t.Errorf("fail to list namesapce:%v", err)
-		return
-	}
-
-	t.Logf("response:%s", string(response))
-
-	rt, _ := rest.TransportFor(&config.Rest)
-	resp, err := rt.RoundTrip(&http.Request{
-		Method: "GET",
-		URL: &url.URL{
-			Scheme: "https",
-			Host:   config.Rest.Host,
-			Path:   fmt.Sprintf("/api/v1/namespaces"),
-		},
-		Header: http.Header{
-			"Authorization": []string{fmt.Sprintf("Bearer %s", (&config.Rest).BearerToken)},
-		},
-	})
+	resp, err := client.Get("/api/v1/namespaces")
 	if err != nil {
 		t.Errorf("fail plain request:%v", err)
 		return
