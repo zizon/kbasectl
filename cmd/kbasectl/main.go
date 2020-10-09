62f85fa3696a723a1d61f7b1c8aab5f61b1d2f87
zizon <ZhiZhongQiu@sf-express.com>
2020-10-09 11:02:12 +0800

    add log verbose

--- cmd/kbasectl/main.go
+++ cmd/kbasectl/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"github.com/spf13/cobra"
 	"github.com/zizon/kbasectl/cmd/kbasectl/generate"
+	"github.com/zizon/kbasectl/pkg/endpoint"
 	"github.com/zizon/kbasectl/pkg/panichain"
 )
 
 func main() {
+	var (
+		logLevel = 0
+	)
 	rootCmd := &cobra.Command{
 		Use: "kbasectl",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			endpoint.SetLogLevel(logLevel)
+		},
 	}
+
+	rootCmd.PersistentFlags().IntVarP(&logLevel, "verbose", "v", 0, "log verbose level")
 
 	rootCmd.AddCommand(
 		generate.NewGenerateComand(),
